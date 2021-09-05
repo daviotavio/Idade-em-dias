06083e7a11e5391e37bf4154977884b250c02041
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-04 22:45:34 -0300

    Add files via upload

---
+++ idade em dias/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace idade_em_dias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            // int dias = 365;
+
+            Console.Write("informe sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Sua idade em dias é: " + idade * 365);
+        }
+    }
+}
